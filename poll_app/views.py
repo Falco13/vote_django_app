dfa92fbec51fb65569cf6e76b172b860a90d8dc4
Falco13 <keanu0608@gmail.com>
2023-03-10 20:01:18 +0200

    fix import

--- poll_app/views.py
+++ poll_app/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic

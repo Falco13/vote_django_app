afd83a8519fa4ed438345bbdd2c9bd0e357f20bb
Falco13 <keanu0608@gmail.com>
2025-01-09 16:57:48 +0200

    you cannot change your vote within 30 days.

--- poll_app/utils.py
+++ poll_app/utils.py
@@ -1,23 +1,24 @@
 from datetime import timedelta
+
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
+from django.urls import reverse
 from django.utils import timezone
-from django.http import HttpResponseForbidden
 from poll_app.models import Question, Vote
 
 
 def can_change_vote(view_func):
     def _wrapped_view(request, *args, **kwargs):
+        slug = kwargs.get('slug')
         question = get_object_or_404(Question, slug=kwargs['slug'])
         user_vote = Vote.objects.filter(voter=request.user, question=question).first()
 
         if user_vote:
-            # Если голос был отдан менее 30 дней назад
             time_difference = timezone.now() - user_vote.created_at
             if time_difference < timedelta(days=30):
-                # Запрещаем изменение голоса
-                return HttpResponseForbidden("You cannot change your vote within 30 days.")
-
-        # Если условия не нарушены, выполняем оригинальную функцию
+                messages.error(request, "You cannot change your vote within 30 days.")
+                return HttpResponseRedirect(reverse('poll_app:results', args=(slug,)))
         return view_func(request, *args, **kwargs)
 
     return _wrapped_view

7d787920bb14d575f071cd1aa56e1fec6bde68d9
Falco13 <keanu0608@gmail.com>
2025-02-23 13:01:17 +0200

    added config.py file

--- poll_app/utils.py
+++ poll_app/utils.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.utils import timezone
 from poll_app.models import Question, Vote
+from poll_app.config import CHANGE_VOTE_DAYS
 
 
 def can_change_vote(view_func):
@@ -15,8 +16,8 @@ def can_change_vote(view_func):
 
         if user_vote:
             time_difference = timezone.now() - user_vote.created_at
-            if time_difference < timedelta(days=30):
-                messages.error(request, "You cannot change your vote within 30 days.")
+            if time_difference < timedelta(days=CHANGE_VOTE_DAYS):
+                messages.error(request, f"You cannot change your vote within {CHANGE_VOTE_DAYS} days.")
                 return HttpResponseRedirect(reverse("poll_app:results", args=(slug,)))
         return view_func(request, *args, **kwargs)
 
